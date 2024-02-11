ee2a6d29c8e3531eb6e4a39dccbea63850748117
A <karrar3035@gmail.com>
2024-02-11 19:46:57 +0300

    Added selectionSort(Array) to Sorter.java

--- src/Sorter.java
+++ src/Sorter.java
@@ -3,4 +3,21 @@
 public class Sorter {
     public static void main(String[] args) {
     }
+    public static void selectionSort(int[] array) {
+        // loop through the array from left to right
+        for (int i = 0; i < array.length - 1; i++) {
+            // find the index of the smallest element in the unsorted part of the array
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // swap the smallest element with the current element
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
+    }
+
 }
